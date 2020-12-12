715ce1b48a36093db7d1362a85bbcc1e9a1ca1c6
Sudhir <sudhirnitnawre@gmail.com>
2020-12-12 23:15:19 +0530

    changes added testjava class

--- TestProject/src/abc/TestJava.java
+++ TestProject/src/abc/TestJava.java
@@ -1,9 +1,13 @@
 package abc;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("", "");
+		WebDriver driver=new ChromeDriver();
 
 	}
 
